df9b5d8391aaeaca0826df96215ab93d44d767cd
samantha <sasalinero@gmail.com>
2022-02-19 17:27:34 +0100

    new

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+       <h1>TO DO LIST</h1>
+              
+      </header>
+      <ul>URGENTE<li></li></ul>
+    </div>
+  );
+}
+
+export default App;
