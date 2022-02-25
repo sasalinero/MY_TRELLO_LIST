e218ab49cb14c42c9f16cbf852787495694d38f3
samantha <sasalinero@gmail.com>
2022-02-25 15:59:58 +0100

    alpha

--- src/components/AddCardorList.js
+++ src/components/AddCardorList.js
@@ -1,5 +1,5 @@
 //Componente para añadir una lista o una tarea nueva
-import{Collapse, fade, makeStyles, Paper, Typography} from "@material-ui/core"//Collapse, Con este componente podemos hacer un acordeon dandole a un boton expandir o no
+import{Collapse, alpha, makeStyles, Paper, Typography} from "@material-ui/core"//Collapse, Con este componente podemos hacer un acordeon dandole a un boton expandir o no
 import { useState } from "react"
 import { AddCardorListText } from "./AddCardorListText";
 
@@ -39,13 +39,19 @@ const useStyle= makeStyles(theme=>({
   root:{
   
      width:"300px",
-     marginTop: theme.spacing(1)
+     marginTop: theme.spacing(1),
   },
+
   addCardorListText: {
 
-    padding:theme.spacing(1,1,1,2)
+    padding: theme.spacing(1, 1, 1, 2),
+    margin: theme.spacing(0, 1, 1, 1),
+    background: "#ebecf0",
+    "&:hover" :{
+      backgroundColor: alpha("#000", 0.25)
+    }
 
-  }
+  }//Con el hover y el fade hacemos que cuando el raton pase por encima se oscurezca #000 pero solo un 25%
 
 }))
 
